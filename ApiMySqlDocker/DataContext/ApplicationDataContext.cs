76559a15bfb32cef2bdc3a2600774499ff092329
Bufaonanet <douglas.bufaonanet@outlook.com>
2021-09-23 14:08:26 -0300

    inicial

---
+++ ApiMySqlDocker/DataContext/ApplicationDataContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiMySqlDocker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiMySqlDocker.DataContext
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
